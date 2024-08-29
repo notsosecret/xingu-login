8a57d7bc279fb3093261509e5002c5bb788a733c
andreas <mail@andreas-eisenberger.de>
2024-08-30 00:40:29 +0200

    small refactor

--- static/script.js
+++ static/script.js
@@ -2,8 +2,6 @@ const button = document.getElementById("openModal")
 button.addEventListener("click", openModal)
 
 function openModal() {
-    const overlay = document.getElementById("overlay")
-    overlay.style.display = "block"
-    const modal = document.getElementById("modal")
-    modal.style.display = "block"
+    const modalWrapper = document.getElementById("modalWrapper")
+    modalWrapper.classList.add("modal-open")
 }
