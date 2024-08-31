c5f67a594242463bf5656851cafc28273ea3ea7a
andreas <mail@andreas-eisenberger.de>
2024-08-31 11:52:57 +0200

    take advantage of chat-gpt for final version

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 Can be started using:
 
 fastapi dev main.py
+
+Start here:
+
+http://127.0.0.1:8000/index
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -15,24 +14,17 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/index")
 def index(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="index.html"
-    )
+    return templates.TemplateResponse(request=request, name="index.html")
+
 
 @app.post("/login")
-def login(
-    email: Annotated[str, Form()],
-    password: Annotated[str, Form()],
-    request: Request
-):
+def login(request: Request, email: Annotated[str, Form()], password: Annotated[str, Form()]):
     if email == "admin@xingu-ad.com" and password == "admin":
         return RedirectResponse("/secret", status.HTTP_302_FOUND)
     else:
-        return templates.TemplateResponse(
-        request=request, name="index.html", 
-        status_code=status.HTTP_401_UNAUTHORIZED, 
-        context={"error": True, "email": email, "password": password}
-    )
+        return templates.TemplateResponse(request=request, name="index.html", status_code=status.HTTP_401_UNAUTHORIZED,
+                                          context={"error": True, "email": email, "password": password})
+
 
 @app.get("/secret")
 def read_secret():
